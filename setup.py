d3a3d618876d2d3a4c4869d6a27cc5973be8b88f
Ben Whittle <benwhittle31@gmail.com>
2017-08-24 18:30:12 -0700

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import os
+
+os.mkdir('data')
+os.mkdir('data/dml')
+os.mkdir('data/html')
+os.mkdir('data/html_discard')
+os.mkdir('data/images')
+os.mkdir('data/permutations')
+
+os.system('touch .env')
